9f21ffc242d5e63d573b485de03d0d20fb2918ee
Victor Martins <vhprestes@gmail.com>
2022-09-23 15:14:46 -0300

    Iniciando o projeto

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 // ...
-
+// comentário pra commit
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
