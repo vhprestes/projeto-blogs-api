82d69634062fbc5bedfd19b73dfc41ff7705b936
Victor Martins <vhprestes@gmail.com>
2022-10-01 20:10:34 -0300

    fix pelo amor de Deus

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,13 +1,13 @@
 // req 4
 const jwt = require('jsonwebtoken');
-const { User } = require('../models');
+// const { User } = require('../models');
 
 const TOKEN_SECRET_KEY = process.env.JWT_SECRET || 'suaSenhaSecreta';
 
-const getByUserIdService = (id) => User.findOne({
-  where: { id },
-  attributes: { exclude: ['password'] },
-});
+// const getByUserIdService = (id) => User.findOne({
+//   where: { id },
+//   attributes: { exclude: ['password'] },
+// });
 
 module.exports = async (req, res, next) => {
   const token = req.header('Authorization');
@@ -15,9 +15,9 @@ module.exports = async (req, res, next) => {
     return res.status(401).json({ message: 'Token not found' });
   } try { 
     const decoded = jwt.verify(token, TOKEN_SECRET_KEY);
-    const user = await getByUserIdService(decoded.id);
-    req.user = user;
-  next();
+    // const user = await getByUserIdService(decoded.id);
+    req.user = decoded.user;
+    next();
   } catch (err) {
   return res.status(401).json({ message: 'Expired or invalid token' });
   }
